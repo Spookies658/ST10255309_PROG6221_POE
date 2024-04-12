66979d73d0a7648ea3ddae87573d9d7308644316
Spookies658 <129881609+Spookies658@users.noreply.github.com>
2024-04-12 13:44:36 +0200

    Creating methods in the recipe class

---
+++ ST10255309_PROG6221_POE/Workings/Recipe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ST10255309_PROG6221_POE.Workings
+{
+    internal class Recipe
+    {
+
+        public void Recepe()
+        {
+            Menu();
+            Create();
+            Delete();
+            View();
+            Exit();
+        }
+
+        private void Menu()
+        {
+            Console.WriteLine("Please select an option from the menu below: \n\n1. Create Recipe\n2. View Recipe\n3. Delete Recipe\nExit");
+        }
+
+        private void Create()
+        {
+
+        }
+
+        private void Delete()
+        {
+
+        }
+
+        private void View()
+        {
+
+        }
+
+        private void Exit()
+        {
+            
+        }
+    }
+}
