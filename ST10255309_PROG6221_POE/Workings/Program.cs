77865be257e14832a110d7468de7f7349ed07354
Spookies658 <129881609+Spookies658@users.noreply.github.com>
2024-04-12 13:26:39 +0200

    Add project files.

---
+++ ST10255309_PROG6221_POE/Workings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10255309_PROG6221_POE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
