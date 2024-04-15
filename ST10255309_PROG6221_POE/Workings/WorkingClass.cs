1d2b75222c101238f89d9b58b86ed256abe6701e
Spookies658 <129881609+Spookies658@users.noreply.github.com>
2024-04-15 16:23:14 +0200

    Added a method to allow the user to scale their ingredient quantity and added comments to the various methods

--- ST10255309_PROG6221_POE/Workings/WorkingClass.cs
+++ ST10255309_PROG6221_POE/Workings/WorkingClass.cs
@@ -9,13 +9,13 @@ namespace ST10255309_PROG6221_POE.Workings
 {
     internal class WorkingClass
     {
-         ArrayList name = new ArrayList();
-         ArrayList ingredient = new ArrayList();
-         ArrayList ingredientName = new ArrayList();
-         ArrayList ingredientQuantity = new ArrayList();
-         ArrayList measurementUnit = new ArrayList();
-         ArrayList numberSteps = new ArrayList();
-         ArrayList stepDescription = new ArrayList();
+        ArrayList name = new ArrayList();
+        ArrayList ingredient = new ArrayList();
+        ArrayList ingredientName = new ArrayList();
+        ArrayList ingredientQuantity = new ArrayList();
+        ArrayList measurementUnit = new ArrayList();
+        ArrayList numberSteps = new ArrayList();
+        ArrayList stepDescription = new ArrayList();
 
         private string recipeName;
         private int ingredientNumber;
@@ -24,8 +24,10 @@ namespace ST10255309_PROG6221_POE.Workings
         private string unitMeasurement;
         private int stepsNumber;
         private string descriptionStep;
+        private string selection;
 
-      
+
+
         public void userRecp()
         {
             NamesOfRecipe();
@@ -33,26 +35,30 @@ namespace ST10255309_PROG6221_POE.Workings
             IngredientProperties();
             OrderSteps();
             StepDescription();
+            Views();
+            ScalingFactor();
         }
 
-
-        public string NamesOfRecipe()
+        //Method to prompt user for the name of the recipe and store it in the array.
+        private string NamesOfRecipe()
         {
             Console.WriteLine("Please enter the name of the recipe: ");
             recipeName = Console.ReadLine();
             name.Add(recipeName);
             return recipeName;
-        } 
+        }
 
-        public int NumberOfIngredients()
-        {
+        //Method to prompt user for the number of ingredients and store it in the array.
+        private int NumberOfIngredients()
+        { 
             Console.WriteLine("Please enter the number of ingredients: ");
             ingredientNumber = Convert.ToInt32(Console.ReadLine());
             ingredient.Add(ingredientNumber);
             return ingredientNumber;
         }
 
-        public void IngredientProperties()
+        //Method to prompt user for the properties of the number of ingredients they entered and store them in the array.
+        private void IngredientProperties()
         {
             
             for (int i = 0; i < ingredientNumber; i++)
@@ -71,7 +77,8 @@ namespace ST10255309_PROG6221_POE.Workings
             }
         }
 
-       public int OrderSteps()
+        //Method to prompt user for the number of steps and store it in the array.
+        private int OrderSteps()
         {
             Console.WriteLine("Please enter the number of steps: ");
             stepsNumber = Convert.ToInt32(Console.ReadLine());
@@ -79,7 +86,8 @@ namespace ST10255309_PROG6221_POE.Workings
             return stepsNumber;
         }
 
-        public void StepDescription()
+        //Method to prompt user for the description of the steps and store it in the array.
+        private void StepDescription()
         {
             for (int i = 0; i < stepsNumber; i++)
             {
@@ -89,7 +97,75 @@ namespace ST10255309_PROG6221_POE.Workings
             }
         }
 
-       
+        //Method to display the recipe, ingredients and steps to the user.
+        private void Views()
+        {
+            
+            Console.WriteLine("\n\nRecipe: " + name[0]);
+            Console.WriteLine("Ingredients: ");
+            for (int i = 0; i < ingredientNumber; i++)
+            {
+                Console.WriteLine(ingredientName[i] + " " + ingredientQuantity[i] + " " + measurementUnit[i]);
+            }
+            Console.WriteLine("Steps: ");
+            for (int i = 0; i < stepsNumber; i++)
+            {
+                Console.WriteLine(stepDescription[i]);
+            }
+       }
 
+        //Method to prompt user if they would like to scale the recipe and by how much.
+        private void ScalingFactor()
+        {
+            Console.WriteLine("\nIf Scaling is required select one of the following:\n " +
+                              "1. Scale by half\n" +
+                              "2. Scale by 2 times\n" + 
+                              "3.Scale by 3 times\n" + 
+                              "4. Not required\n");
+            selection = Console.ReadLine();
+            if(selection == "1")
+            {
+                for (int i = 0; i < ingredientNumber; i++)
+                {
+                    ingredientQuantity[i] = Convert.ToDouble(ingredientQuantity[i]) * 0.5;
+                    Console.WriteLine("Name: " + name[0] + "\n" +
+                                      "Ingredients: " + ingredientName[i] + " " + ingredientQuantity[i] + " " + measurementUnit[i] + "\n" +
+                                      "Steps: " + "\n" +
+                                      stepDescription[i] + "\n");
+                }
+            }
+            else if(selection == "2")
+            {
+                for (int i = 0; i < ingredientNumber; i++)
+                {
+                    ingredientQuantity[i] = Convert.ToDouble(ingredientQuantity[i]) * 2;
+                    Console.WriteLine("Name: " + name[0] + "\n" +
+                                      "Ingredients: " + ingredientName[i] + " " + ingredientQuantity[i] + " " + measurementUnit[i] + "\n" +
+                                      "Steps: " + "\n" +
+                                      stepDescription[i] + "\n");
+                }
+            }
+            else if(selection == "3")
+            {
+                for (int i = 0; i < ingredientNumber; i++)
+                {
+                    ingredientQuantity[i] = Convert.ToDouble(ingredientQuantity[i]) * 3;
+                    Console.WriteLine("Name: " + name[0] + "\n" +
+                                      "Ingredients: " + ingredientName[i] + " " + ingredientQuantity[i] + " " + measurementUnit[i] + "\n" +
+                                      "Steps: " + "\n" +
+                                      stepDescription[i] + "\n");
+                }
+            }
+            else if(selection == "4")
+            {
+                Console.WriteLine("No Scaling required\n");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please try again\n");   
+            }
+
+
+        }
     }
 }
