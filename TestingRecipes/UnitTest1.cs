9ff334666b50eddfdb3103dc4027e4c105fbb641
Spookies658 <129881609+Spookies658@users.noreply.github.com>
2024-05-23 10:18:17 +0200

    Changed framework to 4.8, changed Arrays to Lists and fixed the methods

---
+++ TestingRecipes/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestingRecipes
+{
+    [TestClass]
+    public class WorkingClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
