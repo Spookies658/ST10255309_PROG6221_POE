ea55654215e24d2c8c80c6dd14a46f28ffb000ff
Spookies658 <129881609+Spookies658@users.noreply.github.com>
2024-06-25 16:06:23 +0200

    Fixed previous calories confusion and added information to various amounts of calories. Started making UI designs

---
+++ ST10255309_PROG6221_POE/ST10255309_PROG6221_POE/Workings/Recipe.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ST10255309_PROG6221_POE.Workings
+{
+    /*Ethan Smyth
+    *ST10255309
+    *Group2
+    Troeleson, A. Japikse, P. 2022. Pro C# 10 with .Net 6 Foundational Principals and Practices in Programming. Chamsbersburg, PA, USA.
+    */
+    internal class Recipe
+    {
+
+        WorkingClass workings = new WorkingClass();
+        string option;
+
+        //A method to display a menu popup to the user
+        public void Menu()
+        {
+            Console.WriteLine("Please select an option from the menu below: \n\n1. Create Recipe\n2. Display\n3. Delete\n4. Exit");
+            option = Console.ReadLine();
+            if (option == "1")
+            {
+                Create();
+            }
+            else if (option == "2")
+            {
+                Display();
+            }
+            else if (option == "3")
+            {
+                ClearRecipe();
+            }
+            else if (option == "4")
+            {
+                Exit();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input, please try again");
+                Console.ResetColor();
+                Menu();
+            }
+        }
+
+        //userRecap is called from the WorkingClass so that the user can create a recipe
+        private void Create()
+        {
+           
+            workings.userRecp();
+            Menu();
+        }
+
+        //usrSearch is called from WorkingClass so that the user can search for a recipe
+        private void Display()
+        {
+           
+            workings.Search();
+            Menu();
+        }
+
+
+        //ClearRecipe is called from the WorkingClass so that the user can delete a recipe
+        private void ClearRecipe()
+        {
+            workings.Gone();
+            Menu();
+        }
+
+       
+        //Method to exit the application
+        private void Exit()
+        {
+            Console.WriteLine("Thank you for using the Recipe Application");
+            System.Environment.Exit(0);
+        }
+    }
+}
---
+++ ST10255309_PROG6221_POE/TestingRecipes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingRecipes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("TestingRecipes")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a471d387-9af7-487d-a959-19aadc1f4f68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
